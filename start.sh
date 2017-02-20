a281d89664391d979e66fdfdab7a672f070fb74e
左盐 <huabinglan@163.com>
2017-02-20 15:42:56 +0800

    init

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-supervisor -e 'cs' -x xbuild WebApplication1.sln 
+supervisor -e 'cs' -x xbuild WebApplication1.sln
+
+nohup fastcgi-mono-server4 /applications=/:/home/data/host/zuoyan/dotnet/cms /socket=tcp:127.0.0.1:9000 &
